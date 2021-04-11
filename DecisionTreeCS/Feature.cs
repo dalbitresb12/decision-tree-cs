61af4c9f7577c93ba48ff4c6bb0861191f75f83a
Diego Albitres <diego.albitres@gmail.com>
2021-04-11 00:31:32 -0500

    Added initial Feature class

---
+++ DecisionTreeCS/Feature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTreeCS {
+  class Feature {
+    public string Name {
+      get;
+      private set;
+    }
+    public dynamic Value {
+      get;
+      private set;
+    }
+
+    public bool IsNumeric => Value is decimal || Value is int || Value is float || Value is double;
+
+    public Feature(string name, dynamic value) {
+      Name = name;
+      Value = value;
+    }
+  }
+}
