69574209ecbbcb18e6f35faf364f0ba513c9f15f
Diego Albitres <diego.albitres@gmail.com>
2021-04-21 01:32:06 -0500

    Added StringExtensions utility functions

---
+++ DecisionTreeCS/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTreeCS {
+  public static class StringExtensions {
+    public static string FirstCharToUpper(this string input) {
+      if (string.IsNullOrEmpty(input))
+        return string.Empty;
+
+      char[] str = input.ToCharArray();
+      str[0] = char.ToUpper(str[0]);
+      return new string(str);
+    }
+
+    public static string FirstCharToLower(this string input) {
+      if (string.IsNullOrEmpty(input))
+        return string.Empty;
+
+      char[] str = input.ToCharArray();
+      str[0] = char.ToLower(str[0]);
+      return new string(str);
+    }
+  }
+}
